a872cbeef2a7e19a20bd95cc3b407eaa2bcf5a1f
Jake Hamilton <jake.hamilton@hey.com>
2025-03-20 09:04:26 -0700

    refactor: expose cli via normal flake output

--- flake.nix
+++ flake.nix
@@ -1,7 +1,16 @@
 {
   description = "The command line interface for Nilla.";
 
-  outputs = inputs: {
-    nilla = import ./nilla.nix;
-  };
+  outputs = inputs:
+    let
+      project = import ./nilla.nix;
+    in
+    {
+      packages = {
+        x86_64-linux = rec {
+          nilla = project.config.packages.nilla.build.x86_64-linux;
+          default = nilla;
+        };
+      };
+    };
 }
